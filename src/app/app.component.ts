6a688f9b0d5569adfdfae622a5db840d205787bc
Volodymyr1939r <rudenkovolodymyr086@gmail.com>
2025-03-03 23:52:22 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './head/header/header.component';
+import {FooterComponent} from './foot/footer/footer.component';
+import {ExperienceComponent} from './experience/experience.component';
+import {EducationComponent} from './education/education.component';
+import {ReferencesComponent} from './references/references.component';
+import {SkillsComponent} from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ExperienceComponent, EducationComponent, ReferencesComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
